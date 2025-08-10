48a50cf96c09b1f97f71696c24f199731c590f35
HSilvaa <100692817+HSilvaa@users.noreply.github.com>
2025-08-10 14:02:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.getElementById('main-header');
+  const navLinks = document.getElementById('nav-links');
+  let lastScrollY = window.scrollY;
+
+  const form = document.getElementById('contactForm');
+  if (form) {
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      const name = document.getElementById('nameInput').value;
+
+      localStorage.setItem('userName', name);
+      const name2 = localStorage.getItem('userName');
+
+      this.submit();
+    });
+  }
+
+  window.addEventListener('scroll', () => {
+    const currentScroll = window.scrollY;
+
+    if (window.innerWidth > 768) {
+      if (currentScroll > lastScrollY && currentScroll > 100) {
+        header.classList.add('hide-header');
+      } else {
+        header.classList.remove('hide-header');
+      }
+    } else {
+      header.classList.remove('hide-header');
+    }
+
+    lastScrollY = currentScroll;
+  });
+
+  function toggleMenu() {
+    navLinks.classList.toggle('show');
+  }
+
+  function changeLang(lang) {
+    if (lang === 'es') {
+      window.location.href = 'index.html';
+    } else if (lang === 'en') {
+      window.location.href = 'index-en.html';
+    }
+  }
+});
